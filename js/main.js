3dbef701038bd095b14815db775307b61ff9fbb0
Kostya <rodina9180gaocheng@ukr.net>
2022-04-16 09:44:32 +0200

    completed adaptivity

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ $(function(){
            breakpoint: 481,
            settings: {
              arrows: false,
+             dots: true,
+             autoplay: true,
            }
          }
        ]
